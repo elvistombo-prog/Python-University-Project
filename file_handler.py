56a0b679aa83cf0bafc78aa6eaa846e5e9d445af
Elvis Tombo <elvistombo@gmail.com>
2026-06-26 17:42:01 +0200

    Implement save and load functions for students
    Added functions to save and load student data using CSV.

--- file_handler.py
+++ file_handler.py
@@ -1,1 +1,30 @@
+import csv
+
+FILE_PATH = "data/students.csv"
+
+
+def save_students(students):
+  with open(FILE_PATH, "w", newline="") as file:
+            writer = csv.writer(file)
+
+  for s in students:
+      writer.writerow([s.student_id, s.name, s.email])
+
+
+def load_students():
+    try:
+        students = []
+
+        with open(FILE_PATH, "r") as file:
+            reader = csv.reader(file)
+
+            for row in reader:
+                students.append(row)
+
+        return students
+
+    except FileNotFoundError:
+        print("No saved data found.")
+        return []
+
 
